b654c4199b2e38aad66e64d4d3c87a5b427d5dd2
Robert Kowalski <rok@kowalski.gd>
2018-09-24 15:46:15 +0100

    ws2: submit orders
    proxy accepts ws v2 format now, sends stub `te` message on order
    insert

--- example-ws-proxy.js
+++ example-ws-proxy.js
@@ -9,6 +9,14 @@ const WebSocket = require('ws')
 
   const ws = new WebSocket('ws://localhost:8888')
 
+  const auth = () => {
+    const msg = JSON.stringify({
+      event: 'auth',
+      id: '1'
+    })
+    ws.send(msg)
+  }
+
   ws.on('open', () => {
     const msg = JSON.stringify({
       event: 'subscribe',
@@ -17,9 +25,27 @@ const WebSocket = require('ws')
     })
 
     ws.send(msg)
+
+    auth()
   })
 
   ws.on('message', (data) => {
     console.log(data)
   })
+
+  setTimeout(() => {
+    const order = JSON.stringify([
+      0,
+      'on',
+      null,
+      {
+        'type': 'EXCHANGE LIMIT',
+        'symbol': 'BTCUSD',
+        'amount': '1.0',
+        'price': '1'
+      }
+    ])
+
+    ws.send(order)
+  }, 800)
 })()
