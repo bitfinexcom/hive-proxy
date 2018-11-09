9ad8d601a3b51ab373d0a49cb77b146f0f0a6082
Robert Kowalski <rok@kowalski.gd>
2018-11-09 15:48:02 +0100

    only use gateway as entry point

--- test/setup-engine.js
+++ test/setup-engine.js
@@ -1,30 +1,27 @@
 'use strict'
 
+const { endpoint } = require('../config/ws.conf.json')
 const Sock = require('../lib/socket-base.js')
 
 function setup () {
   return new Promise((resolve) => {
     const s = new Sock({
-      gateway: 'ipc:///tmp/proxy0',
-      order0: 'ipc:///tmp/order0',
-      user0: 'ipc:///tmp/user0'
+      gateway: endpoint
     })
 
-    s.send('order0', ['test_engine_reset', []])
-    s.send('user0', ['test_engine_reset', []])
     s.send('gateway', ['set_gw_status', { 'trigger_tickers': true, 'trigger_liq': true }])
 
-    s.send('user0', ['update_user_conf', [1, { 'ccys_margin': ['USD', 'BTC', 'ETH', 'LTC', 'JPY', 'EUR'], 'margin_version': 2 }]])
-    s.send('user0', ['set_wallet_balance', [1, 'exchange', 'USD', '1000.00', null, null]])
-    s.send('user0', ['set_wallet_balance', [1, 'exchange', 'BTC', '1000.00', null, null]])
-    s.send('user0', ['set_wallet_balance', [1, 'trading', 'USD', '500.00', null, null]])
-    s.send('user0', ['set_wallet_balance', [1, 'trading', 'BTC', '600.00', null, null]])
+    s.send('gateway', ['update_user_conf', [1, { 'ccys_margin': ['USD', 'BTC', 'ETH', 'LTC', 'JPY', 'EUR'], 'margin_version': 2 }]])
+    s.send('gateway', ['set_wallet_balance', [1, 'exchange', 'USD', '1000.00', null, null]])
+    s.send('gateway', ['set_wallet_balance', [1, 'exchange', 'BTC', '1000.00', null, null]])
+    s.send('gateway', ['set_wallet_balance', [1, 'trading', 'USD', '500.00', null, null]])
+    s.send('gateway', ['set_wallet_balance', [1, 'trading', 'BTC', '600.00', null, null]])
 
-    s.send('user0', ['update_user_conf', [2, { 'ccys_margin': ['USD', 'BTC', 'ETH', 'LTC', 'JPY', 'EUR'], 'margin_version': 2 }]])
-    s.send('user0', ['set_wallet_balance', [2, 'exchange', 'USD', '1000.00', null, null]])
-    s.send('user0', ['set_wallet_balance', [2, 'exchange', 'BTC', '1000.00', null, null]])
-    s.send('user0', ['set_wallet_balance', [2, 'trading', 'USD', '500.00', null, null]])
-    s.send('user0', ['set_wallet_balance', [2, 'trading', 'BTC', '600.00', null, null]])
+    s.send('gateway', ['update_user_conf', [2, { 'ccys_margin': ['USD', 'BTC', 'ETH', 'LTC', 'JPY', 'EUR'], 'margin_version': 2 }]])
+    s.send('gateway', ['set_wallet_balance', [2, 'exchange', 'USD', '1000.00', null, null]])
+    s.send('gateway', ['set_wallet_balance', [2, 'exchange', 'BTC', '1000.00', null, null]])
+    s.send('gateway', ['set_wallet_balance', [2, 'trading', 'USD', '500.00', null, null]])
+    s.send('gateway', ['set_wallet_balance', [2, 'trading', 'BTC', '600.00', null, null]])
 
     setTimeout(() => {
       s.close()
